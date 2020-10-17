15948992acaa604a449bf368e9f5d94af9cb2fe2
Samar <samar1995aziz@gmail.com>
2020-10-17 07:24:16 +0300

    Use setState() to update the state of the image and use random method to create random Q

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
@@ -23,15 +24,17 @@ class BallPage extends StatelessWidget {
     }
 
     class _BallState extends State<Ball> {
+  var ballNumber=1;
       @override
       Widget build(BuildContext context) {
         return Center(
           child:FlatButton(
-              child: Image.asset('images/ball1.png'),
+              child: Image.asset('images/ball$ballNumber.png'),
             onPressed:(){
-        print('I got clicked ');
+               setState(() {
+                 ballNumber=Random().nextInt(4)+1;
+               });
         }
-
           ),
         );
       }
