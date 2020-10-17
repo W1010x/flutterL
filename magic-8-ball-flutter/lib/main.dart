c8538b08f22a871f4a2a02d7dde864572c9cbe1d
Samar <samar1995aziz@gmail.com>
2020-10-17 06:58:49 +0300

    After adding statefull and stateless widget

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
-    );
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+class BallPage extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          backgroundColor: Colors.indigo,
+          appBar: AppBar(
+            title: Text('Ask me any thing'),
+            backgroundColor: Colors.blueGrey,
+          ),
+            body:Ball(),
+        );
+      }
+    }
+    class Ball extends StatefulWidget {
+      @override
+      _BallState createState() => _BallState();
+    }
+
+    class _BallState extends State<Ball> {
+      @override
+      Widget build(BuildContext context) {
+        return Center(
+          child:Image.asset('images/ball1.png'),
+        );
+      }
+    }
+    
