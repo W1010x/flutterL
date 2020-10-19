5c22352dcc460419b35f1a45337696e57d6a4554
Samar <samar1995aziz@gmail.com>
2020-10-19 10:29:04 +0300

    create new function that return expanded , and delete repeated lines

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,9 +4,20 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
   void playSound(int soundNumber){ // create a function to delete the duplicate line code
     final player= AudioCache();
     player.play('note$soundNumber.wav');
+  }
+ Expanded buildKey({Color color, int soundNumber}){//Create a new function to delete all repeated lines return expanded
+    return Expanded(
+      child:FlatButton(
+        color:color,
+        onPressed: (){
+          playSound(soundNumber);
+        },
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -17,55 +28,13 @@ class XylophoneApp extends StatelessWidget {
           child:Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,// help us to stretch the button over the sreen
             children: [
-              Expanded(
-                child:FlatButton(color:Colors.lightGreen,
-                  onPressed: (){
-                  playSound(1);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(color:Colors.lightBlueAccent,
-                  onPressed: (){
-                   playSound(2);
-                  },
-                ),
-              ),
-              Expanded(
-                child :FlatButton(color:Colors.blue,
-                  onPressed: (){
-                    playSound(3);
-                  },
-                ),
-              ),
-              Expanded(
-                child :FlatButton(color:Colors.teal,
-                  onPressed: (){
-                    playSound(4);
-                  },
-                ),
-              ),
-              Expanded(
-                child:FlatButton(color:Colors.pink,
-                  onPressed: (){
-                    playSound(5);
-                  },
-                ),
-              ),
-              Expanded(
-                child:FlatButton(color:Colors.grey,
-                  onPressed: (){
-                    playSound(6);
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(color:Colors.lime,
-                  onPressed: (){
-                    playSound(7);
-                  },
-                ),
-              ),
+              buildKey(color:Colors.red, soundNumber:1),
+              buildKey(color:Colors.green, soundNumber:2),
+              buildKey(color:Colors.grey, soundNumber:3),
+              buildKey(color:Colors.blue, soundNumber:4),
+              buildKey(color:Colors.lime, soundNumber:5),
+              buildKey(color:Colors.pink, soundNumber:6),
+              buildKey(color:Colors.lightBlueAccent, soundNumber:7),
             ],
           ),
 
