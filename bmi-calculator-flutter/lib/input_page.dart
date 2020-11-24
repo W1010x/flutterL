99c64ba7c3039249fb56744b2a2765ff5e1d54b2
Samar <samar1995aziz@gmail.com>
2020-11-24 21:30:40 +0300

    customising Widget Using Themes

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -84,18 +84,26 @@ class _InputPageState extends State<InputPage> {
                   style: kLableTextStyle,)
               ],
             ),
-            Slider(
-                value:height.toDouble(),
-                   min: 120.0,
-                max:220.0,
-              activeColor:Color(0xFFEB1555),
-              inactiveColor: Color(0xFF8DE98),
-              onChanged: (double newValue){
-                  setState(() {
-                    height=newValue.round();
-                  });
-              },
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: Colors.white,
+                thumbColor: Color(0xFFEB1555),
+                overlayColor: Color(0x29EB1555),
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                overlayShape: RoundSliderOverlayShape(overlayRadius: 24.0),
+              ),
+              child: Slider(
+                  value:height.toDouble(),
+                     min: 120.0,
+                  max:220.0,
+                inactiveColor: Color(0xFF8DE98),
+                onChanged: (double newValue){
+                    setState(() {
+                      height=newValue.round();
+                    });
+                },
 
+              ),
             )
             ],
           ),
