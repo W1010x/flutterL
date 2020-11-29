c9a7fea1b50203c88ccc384ccdbefedd6f5301fc
Samar <samar1995aziz@gmail.com>
2020-11-30 01:49:49 +0300

    import geolacator package and use it with function

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation()async{
+    Position position =
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
